5710744c08347dcdfdeb37c70d81b92e3ce7638b
Ajith <ajith@graniteriverlabs.in>
2020-11-03 10:16:13 +0530

    1.Changed port number

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -8,8 +8,8 @@ export const PANELS = [{ title: "Connection Setup", name: "ConnectionSetup" },
 { title: "Help", name: "Help" },
 ];
 
-// const URL_Server_Address = "http://" + window.location.hostname + ":" + window.location.port + "/"
-const URL_Server_Address = "http://" + window.location.hostname + ":5001/"
+const URL_Server_Address = "http://" + window.location.hostname + ":" + window.location.port + "/"
+// const URL_Server_Address = "http://" + window.location.hostname + ":5001/"
 export const URL_base = URL_Server_Address + "api/";
 export const URL_ConnectionSetup = URL_base + PANELS[0].name + "/";
 export const URL_ProductCapability = URL_base + PANELS[1].name + "/";
